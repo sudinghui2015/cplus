627b71230ccddbd98f821d68bfe5eb6b4c1c3345
苏定辉 <511524899@qq.com>
2017-06-08 00:39:18 +0800

    增加carrots.cpp

---
+++ study/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: sudinghui
+ *
+ * Created on 2017年6月8日, 上午12:33
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    //return 0;
+    int carrots;
+    
+    carrots = 25;
+    cout << "I have";
+    cout << carrots;
+    cout << " carrots ";
+    cout << endl;
+    carrots = carrots - 1;
+    cout << "Crunch,crunch.Now I have " << carrots << " carrots." << endl;
+    return 0;
+}
+
